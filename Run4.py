d1913afbe5d2e3ecea874c106bc80d14a37fba52
Goats1 <thecodinggoats@gmail.com>
2024-08-28 16:28:01 -0500

    Nella Edited run four

---
+++ Run4.py
@@ -0,0 +1,6 @@
+from robotClass import Robot
+Robot.straight(1.2)   # picks up krill number one and two
+Robot.turn(72)        # turns to pick up krill number three
+Robot.straight(.1)    # picks up Krill number three
+Robot.turn(-3)        
+Robot.turn(-45)       # turns to dump krill
