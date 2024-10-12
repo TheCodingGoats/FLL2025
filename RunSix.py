2d8f868435bfff749e79ed3fe6c5b301a670e640
Goats1 <thecodinggoats@gmail.com>
2024-10-11 21:27:55 -0500

    Run Six - Nella

---
+++ RunSix.py
@@ -0,0 +1,14 @@
+import robotClass
+
+robo = robotClass.Robot()
+
+robo.straight (.19, 200)
+robo.turn (-36.5, 200)
+robo.straight (1.08, 200)
+robo.turn (34.4, 200)
+robo.straight (.05, 200)
+robo.straight (-.02, 200)
+# robo.straight (-.7, 200)
+# robo.turn (-50, 200)
+# robo.straight (.07, 200)
+# robo.frontArm (1.3, 200)
--- changeShipingLanes.py
+++ changeShipingLanes.py
@@ -4,7 +4,7 @@ robo = robotClass.Robot()
 
 robo.frontArm(0.06, -40, Wait = False)
 robo.straight(.69, 800, 400)
-robo.turn(40, 700, 400)
+robo.turn(40, 75, 400)
 robo.straight(0.1, 200, 800)
 robo.frontArm( .5, -150, Wait = False)
 robo.turn( 10, 10)
