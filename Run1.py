f91b7d33adb224fa58215b3994df06d6c7966367
Goats2 <thecodinggoats2@gmail.com>
2024-10-22 14:15:58 -0500

    Miles - changes to run 1

--- Run1.py
+++ Run1.py
@@ -4,26 +4,46 @@ from pybricks.tools import wait
 
 robo = robotClass.Robot()
 robo.straight(.4, 200, 200)
-robo.frontArm(.48, -150, False)
+robo.frontArm(.46, -150, False)
 wait(500)
 robo.straight(.36, 100, 300)
 robo.frontArm(.2, 75)
 robo.straight(-.15, 200)
-robo.turn(65, 400)
-robo.straight(.3, 200)
-robo.straight(-.1,200)
+robo.turn(48, 400)
+robo.straight(.56, 75)
+robo.turn(-40, 100)
+robo.straight(.2, 400)
+robo.turn(-47, 200)
+robo.straight(.1)
+robo.frontArm(.5, 400)
+
+
+# robo.straight(.3, 200)
+# robo.turn(-10, 400)
+# robo.straight(-.2, 150)
+# robo.turn(-27, 400)
+# robo.straight(.3, 150)
+# robo.turn(25,400)
+# robo.straight(.2,200)
+# robo.turn (-15,400)
+# robo.straight(-.2)
+# robo.turn(-20,400)
+# robo.straight(-.1,200)
 #First coral
-robo.turn(-25, 200, 50)
-robo.straight(-.05)
-robo.turn(-15, 200, 50)
-robo.straight(.2, 200) 
-#second coral
-robo.turn(30, 400)
-robo.straight(.25, 200) 
-#Water Sample
-robo.turn(-40, 400)
-robo.straight(.17, 200) 
-#krill
+
+
+
+# robo.turn(-25, 200, 50)
+# robo.straight(-.05)
+# robo.turn(-15, 200, 50)
+# robo.straight(.2, 200) 
+# #second coral
+# robo.turn(30, 400)
+# robo.straight(.25, 200) 
+# #Water Sample
+# robo.turn(-40, 400)
+# robo.straight(.17, 200) 
+# #krill
 
 
 
