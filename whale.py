1a14611fa9ba24569ea5dc211389576455dfc7a5
Goats2 <thecodinggoats2@gmail.com>
2024-10-23 14:01:05 -0500

    Hudson - Whale edit

---
+++ whale.py
@@ -0,0 +1,28 @@
+from robotClass import Robot
+
+# collecting the krill
+Robot.straight(-250, 100)
+Robot.turn(50, 20)
+Robot.straight(-100, 100)
+
+
+
+
+
+
+
+
+
+
+
+# # driving to the whale
+# Robot.straight(-400, 100)
+# Robot.turn(37, 10)
+
+# # drop off at the whale
+# Robot.straight(-120, 100)
+
+# # driving to sonar discovery
+# Robot.straight(180, 100)
+# Robot.turn(45)
+
