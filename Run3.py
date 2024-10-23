98b0cadfe1f8d8000f33e21baa5fd64698128ffd
Goats2 <thecodinggoats2@gmail.com>
2024-10-23 16:17:21 -0500

    Hudson - Changes on team meeting 10.23.2024

--- Run3.py
+++ Run3.py
@@ -5,17 +5,18 @@ robo = robotClass.Robot()
 
 
 robo.straight(.05, 200)
-robo.turn(-45, 40)
-robo.straight(.62, 200)
+robo.turn(-44, 40)
+robo.straight(.65, 200)
 robo.straight(-.35, 200)
-robo.turn(33, 40)
-robo.frontArm(100, -500,Wait = False)
+robo.turn(35, 40)
+robo.frontArm(100, -600,Wait = False)
 robo.straight(.4, 200)
-robo.turn(15)
+robo.turn(10)
 robo.straight(.5, 200)
-robo.turn(42, 40)
+robo.straight(-.1, 200)
+robo.turn(35, 40)
 while robo.leftSensor("reflection") > 20 and robo.rightSensor("reflection"):
-    robo.straight(.05, 200)
+    robo.straight(.1, 200)
 wait(10000)
 
 
