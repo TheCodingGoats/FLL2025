8b1461ffeea5af1c2bb0ee3dd321649026a8f15f
Goats1 <thecodinggoats@gmail.com>
2024-10-23 15:48:20 -0500

    miles - run one

--- Run1.py
+++ Run1.py
@@ -10,58 +10,27 @@ robo.straight(.36, 100, 300)
 robo.frontArm(.2, 75)
 robo.straight(-.15, 200)
 robo.turn(65, 400)
-robo.straight(.3, 200)
-robo.straight(-.1,200)
+robo.straight(.24, 200)
 #First coral
 robo.turn(-25, 200, 50)
 robo.straight(-.05)
 robo.turn(-15, 200, 50)
 robo.straight(.2, 200) 
 #second coral
-robo.turn(30, 400)
-robo.straight(.25, 200) 
+robo.turn(20, 400)
+robo.straight(.3, 200) 
 #Water Sample
 robo.turn(-40, 400)
-robo.straight(.17, 200) 
+robo.straight(.12, 200) 
 #krill
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# robo.straight(distance = .21, speed = 500)
-# robo.frontArm(angle = .22, speed = -50)
-
-
-# robo.frontArm(angle = .32, speed = -35,  Wait = False)
-# robo.straight(distance=.36, speed = 40)
-
-# robo.frontArm(angle = .05, speed = -200)
-# robo.frontArm(angle = .5, speed = 200)
-
-# robo.straight(distance=-.5, speed = -200)
-
-# robo.straight(distance=.1, speed = 20)
-# robo.turn(angle=5, speed=50)
+robo.turn(-45, 400)
+robo.straight(.12, 200)
+robo.frontArm(1, 400)
+robo.frontArm(-1, 400)
+robo.straight(-.2, 200)   
+robo.turn(-20) 
+robo.straight(.1, 200) 
+robo.frontArm(1, 400)
+robo.frontArm(-1, 400)                                                                                     
+# robo.turn(-15, 400)
+# robo.straight(.759, 200)  
