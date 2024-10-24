abce94f067bfc3939e3c7910fc8959caa5b0451c
Goats1 <thecodinggoats@gmail.com>
2024-10-24 18:56:41 -0500

    Levi - Thursday work

--- Run1.py
+++ Run1.py
@@ -1,36 +1,30 @@
 import robotClass
 from pybricks.tools import wait
 
-
 robo = robotClass.Robot()
-robo.straight(.4, 200, 200)
-robo.frontArm(.46, -150, False)
-wait(500)
+
+# lift coral tree
+robo.straight(.28, 200, 200)
+robo.frontArm(.56, -150, False)
+wait(650)
 robo.straight(.36, 100, 300)
 robo.frontArm(.2, 75)
 robo.straight(-.15, 200)
+# collect first coral
 robo.turn(65, 400)
-robo.straight(.24, 200)
-#First coral
-robo.turn(-25, 200, 50)
-robo.straight(-.05)
-robo.turn(-15, 200, 50)
-robo.straight(.2, 200) 
-#second coral
-robo.turn(20, 400)
-robo.straight(.3, 200) 
-#Water Sample
-robo.turn(-40, 400)
-robo.straight(.12, 200) 
-#krill
-robo.turn(-45, 400)
-robo.straight(.12, 200)
-robo.frontArm(1, 400)
-robo.frontArm(-1, 400)
-robo.straight(-.2, 200)   
-robo.turn(-20) 
-robo.straight(.1, 200) 
-robo.frontArm(1, 400)
-robo.frontArm(-1, 400)                                                                                     
-# robo.turn(-15, 400)
-# robo.straight(.759, 200)  
+robo.straight(.21, 200)
+robo.turn(-21, 200, 50)
+robo.straight(.32, 200)
+robo.turn(-34, 400)
+robo.straight(.2, 200)
+robo.turn(-48, 400)
+# robo.straight(.12, 200)
+# robo.turn(-48, 400)
+# robo.straight(.12, 200)
+# robo.frontArm(1, 400)
+# robo.frontArm(-1, 400)
+# robo.straight(-.2, 200)   
+# robo.turn(-14) 
+# robo.straight(.13, 200)
+# robo.frontArm(1, 400)
+# robo.frontArm(-1, 400)
---
+++ runOrganizer.py
@@ -0,0 +1,2 @@
+from pybricks.parameters import Button
+runs = ["1", "2","3","4","5",]
