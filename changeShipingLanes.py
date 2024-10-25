2111d35c5e9f9c739cf36d9f601e3575ebba8c31
Goats1 <thecodinggoats@gmail.com>
2024-10-24 19:07:01 -0500

    Miles - gitcraken is messing us up!!!!!!!!!!!

---
+++ changeShipingLanes.py
@@ -0,0 +1,14 @@
+import robotClass
+from pybricks.tools import wait
+robo = robotClass.Robot()
+
+robo.frontArm(0.06, -40, Wait = False)
+robo.straight(.69, 800, 400)
+robo.turn(40, 75, 400)
+robo.straight(0.1, 200, 800)
+robo.frontArm( .5, -150, Wait = False)
+robo.turn( 10, 10)
+wait(500)
+robo.straight(-0.1, 800, 800)
+robo.turn(-40, 700, 400)
+robo.straight(-.7, 800, 700)
