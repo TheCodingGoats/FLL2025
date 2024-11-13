d255e1ca1a47368c6445b3229609cbe0a4fcfb82
Goats2 <thecodinggoats2@gmail.com>
2024-11-13 17:01:24 -0600

    Hudson - Changes

--- Run3.py
+++ Run3.py
@@ -7,24 +7,25 @@ def run3():
     robo = robotClass.Robot()
 
     # pushing unexpected encounter
-    robo.straight(.05, 300)
+    robo.straight(.05, 200)
     robo.turn(-43, 40)
-    robo.straight(.64, 300)
-    robo.straight(-.35, 300)
+    robo.straight(.64, 200)
+    robo.straight(-.35, 200)
     # collecting 1st krill
     robo.turn(38, 40)
-    robo.frontArm(100, -650, Wait = False)
-    robo.straight(.4, 300)
+    robo.frontArm(100, -800, Wait = False)
+    robo.straight(.4, 200)
     # collecting 2nd krill
-    robo.turn(5)
-    robo.straight(.555, 300)
+    robo.turn(6)
+    robo.straight(.56, 200)
     # collecting 3rd krill & dumping in whale & collecting plankton sample
-    robo.turn(46, 40, 40) # Was 46
+    robo.turn(46, 40, 40)
     while robo.leftSensor("reflection") > 20 and robo.rightSensor("reflection"):
-        robo.straight(.1, 100, 50)
-    robo.straight(-.05, 300)
+        robo.straight(.1, 50, 50)
+    robo.straight(-.05, 200)
     robo.frontArm(2, 800)
     # going home
+<<<<<<< Updated upstream
     robo.straight(-.4, 300)
     robo.turn(-15, 200, 499)
     robo.straight(-.2, 700, 499)
@@ -33,4 +34,10 @@ def run3():
     robo.straight(-1.35, 700, 499)
 =======
     robo.straight(-1.35, 700, 499)
+>>>>>>> Stashed changes
+=======
+    robo.straight(-.5, 200)
+    robo.turn(-80, 499)
+    robo.straight(-1.35, 700, 499)
+
 >>>>>>> Stashed changes
